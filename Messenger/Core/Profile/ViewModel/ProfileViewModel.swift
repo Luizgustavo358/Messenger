487ab5e0a19f485f25d18543a820445be71597f6
Luizgustavo358 <luiz.gustavo358@gmail.com>
2025-02-17 20:40:53 -0300

    selection profile photo

---
+++ Messenger/Core/Profile/ViewModel/ProfileViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  ProfileViewModel.swift
+//  Messenger
+//
+//  Created by Luiz Gustavo Bragança dos Santos on 17/02/25.
+//
+
+import SwiftUI
+import PhotosUI
+
+class ProfileViewModel: ObservableObject {
+    @Published var selectedItem: PhotosPickerItem? {
+        didSet {
+            Task {
+                try await loadImage()
+            }
+        }
+    }
+    
+    @Published var profileImage: Image?
+    
+    func loadImage() async throws {
+        guard let item = selectedItem else {
+            return
+        }
+        
+        guard let imageData = try await item.loadTransferable(type: Data.self) else {
+            return
+        }
+        
+        guard let uiImage = UIImage(data: imageData) else {
+            return
+        }
+        
+        self.profileImage = Image(uiImage: uiImage)
+    }
+}
